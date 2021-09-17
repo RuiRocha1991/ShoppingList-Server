c95823df1af4513cbd423563d32e7fc2d9ccb3ff
Rocha <rui.rocha@getxray-app>
2021-09-17 17:41:54 +0100

    SL-5

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+// Load Config
+dotenv.config({path: './config/config.env'});
+
+const app = express();
+const router = express.Router();
+
+
+// Routes
+app.use('/', router.get('/',(req, res) => {
+  res.status(200).json({message: "Welcome guys to GET"}).send();
+}));
+app.use('/', router.post('/',(req, res) => {
+  res.status(200).json({message: "Welcome guys to POST"}).send();
+}));
+app.use('/', router.put('/',(req, res) => {
+  res.status(200).json({message: "Welcome guys to PUT"}).send();
+}));
+app.use('/', router.delete('/',(req, res) => {
+  res.status(200).json({message: "Welcome guys to DELETE"}).send();
+}));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
