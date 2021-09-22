1101e336265d05bd6c7c80895118a49e552ab7ce
Rocha <rui.rocha@getxray-app>
2021-09-22 19:00:21 +0100

    SL-26
    remove unnecessary user information

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,8 +3,8 @@
 exports.getUserOnSuccessSignIn = async (req, res) =>
 {
   try{
-    const {googleId, displayName, _id, image} = req.user;
-    res.status(200).json({user: {googleId, displayName, _id, image}});
+    const {displayName, image} = req.user;
+    res.status(200).json({user: {displayName, image}});
   } catch(e){
     res.status(500).send({
       message:'Error request',
