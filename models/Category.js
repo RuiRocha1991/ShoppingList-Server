f5e69ac061267adfa770adac0e8813ee22316f3c
Rocha <rui.rocha@getxray-app>
2021-09-24 18:06:53 +0100

    SL-27
    Add category model

---
+++ models/Category.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const CategorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+});
+
+module.exports = mongoose.model('Category', CategorySchema);
