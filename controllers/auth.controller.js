641d250f031bf6073cc3ccfe75c207deed4aca2c
Rocha <rui.rocha@getxray-app>
2021-09-28 22:44:59 +0100

    auth

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,8 +2,9 @@
 
 exports.getUserOnSuccessSignIn = async (req, res) => {
   try{
-    const {displayName, image} = req.user;
-    res.status(200).json({user: {displayName, image}});
+    console.log(req.headers);
+    const {displayName, image, _id} = req.user;
+    res.status(200).json({user: {displayName, image, id:_id}});
   } catch(e){
     res.status(500).send({
       message:'Error request',
