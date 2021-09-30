b8324721910d139beb0e604ac6385fd841146d4b
Rocha <rui.rocha@getxray-app>
2021-09-30 18:19:19 +0100

    SL-33

--- controllers/items.controller.js
+++ controllers/items.controller.js
@@ -3,7 +3,7 @@ const itemRepository = require('../repository/item.repository');
 exports.getItemsByUserId = async (req, res) => {
   try {
     // const items = await itemRepository.getAllItemsByUserId(res.locals.user._id);
-    const items = [{_id: 123, name:"test1"},{_id: 456, name:"test2"}]
+    const items = [{_id: 123, name:"test1"},{_id: 456, name:"test2"},{_id: 345456, name:"test3"},{_id: 34534534, name:"test4"},{_id: 345345235, name:"test5"}]
     res.status(200).json({items: items, token: res.locals.token});
   } catch (err) {
     console.error(err);
